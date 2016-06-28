fcabe4c1d14c2ecfbce2b6ae748c83356b9b55dd
mangan <mangan@localhost>
2016-06-28 10:13:42 +0200

    version increase

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ from distutils.core import setup
 setup(
     name="ksbuild",
     description="Anaconda kickstart grinder",
-    version="1.0",
+    version="1.2",
     author="mangan",
     py_modules=["ksbuild"])
